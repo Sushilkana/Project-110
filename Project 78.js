a2a6da0291ce8fa2003bc88600ea6a063054366d
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2021-10-15 21:53:33 +0530

    Delete Project 78.js

--- Project 78.js
+++
@@ -1,7 +0,0 @@
-var family = [];
-
-function Add() {
-    var member = document.getElementById("member").value;
-    family.push(member + "<br>");
-    document.getElementById("members").innerHTML = family;
-}
