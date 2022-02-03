469dcbe59305f3407d2026a7d73d4d56695c7010
Sushilkana <86962755+Sushilkana@users.noreply.github.com>
2022-02-03 22:13:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -27,12 +27,69 @@ function check() {
     console.log(classifier);
 }
 
-function gotResult(error, results) {
-    if (error) {
+var perdiction1 = "";
+var perdiction2 = "";
+
+function Speak(){
+    var synth = window.speechSynthesis;
+    speakData1 = "The first prediction is" + perdiction1;
+    speakData2 = "And the secound perdiction is " + perdiction2;
+    utterThis = new SpeechSynthesisUtterance(speakData1 + speakData2);
+    synth.speak(utterThis);
+}
+
+function check(){
+    img = document.getElementById("output_img");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,result){
+    if (error){
         console.log(error);
-    }else {
-        console.log(results);
-        document.getElementById("result_name_1").innerHTML = results[0].label;
-        document.getElementById("result_name_2").innerHTML = results[1].label;
+    } else {
+        document.getElementById("result_name_1").innerHTML = result[0].label;
+        document.getElementById("result_name_2").innerHTML = result[1].label;
+
+        perdiction1 = result[0].label
+        perdiction2 = result[1].label
+        Speak()
+
+        if (result[0].label == "Amazing"){
+            document.getElementById("result_emoji_1").innerHTML = "&#128076;"
+        } 
+        else if (result[0].label == "Best"){
+            document.getElementById("result_emoji_1").innerHTML = "&#128077;"
+        }
+        else if (result[0].label == "Bad"){
+            document.getElementById("result_emoji_1").innerHTML = "&#128078;"
+        }
+        else if (result[0].label == "Yo Yo"){
+            document.getElementById("result_emoji_1").innerHTML = "&#129304;"
+        }
+        else if (result[0].label == "Chill"){
+            document.getElementById("result_emoji_1").innerHTML = "&#129305;"
+        }
+        else if (result[0].label == "Loser"){
+            document.getElementById("result_emoji_1").innerHTML = "&#128070;"
+        }
+
+        if (result[1].label == "Amazing"){
+            document.getElementById("result_emoji_2").innerHTML = "&#128076;"
+        } 
+        else if (result[1].label == "Best"){
+            document.getElementById("result_emoji_2").innerHTML = "&#128077;"
+        }
+        else if (result[1].label == "Bad"){
+            document.getElementById("result_emoji_2").innerHTML = "&#128078;"
+        }
+        else if (result[1].label == "Yo Yo"){
+            document.getElementById("result_emoji_2").innerHTML = "&#129304;"
+        }
+        else if (result[1].label == "Chill"){
+            document.getElementById("result_emoji_2").innerHTML = "&#129305;"
+        }
+        else if (result[1].label == "Loser"){
+            document.getElementById("result_emoji_2").innerHTML = "&#128070;"
+        }
     }
 }
